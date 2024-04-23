6b59867b24940cb27dcc003acefdc4a1943a024f
Arief <mrarief0110@gmail.com>
2024-04-23 13:00:17 +0530

    learning about keywords and variable decleration

---
+++ keywords/keywords.py
@@ -0,0 +1,18 @@
+import keyword
+
+# keywords
+print(keyword.kwlist) 
+#35 keywords in python 3.12.3
+# to find python version use terminal >python --version
+
+
+# OutPut
+#['False', 'None', 'True', 'and', 'as', 'assert', 'async', 'await', 'break', 'class', 'continue',
+# 'def', 'del', 'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in',
+# 'is', 'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+
+
+# soft keywords keywords
+print(keyword.softkwlist)
+
+
---
+++ variable/variable.py
@@ -0,0 +1,13 @@
+a="Arief"
+b="A"
+print( a + " " +b ) # o/p Arief A
+print( a + b ) # o/p AriefA
+
+# declering two integires
+c,d = 1,2
+e = (c + d) # sum of the two int in single variable
+
+print( c + d ) # o/p 3
+print( e ) # o/p 3
+print( d ) # o/p 2
+
