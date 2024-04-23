7bb9c8df9bf13dec94233304f9ca9f53c13c08e6
Arief <mrarief0110@gmail.com>
2024-04-23 14:46:50 +0530

    learned about how to assign value to an variable

--- variable/variable.py
+++ variable/variable.py
@@ -11,3 +11,18 @@ print( c + d ) # o/p 3
 print( e ) # o/p 3
 print( d ) # o/p 2
 
+#variable assigning acceptable
+# camel case 
+# a-z , A-Z, '_'
+
+firstName = "Mohamed"
+first_name ="Mohamed"
+a1 = 5
+a_1 = 7
+_a = "str"
+
+#variable assigning NOT_acceptable
+# no special characters are allowed
+# 1 = 5
+# 1a = 12
+# $a=12
